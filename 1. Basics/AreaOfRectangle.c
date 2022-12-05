569c01b645e1be43a72e82f98d6be3acaf5e01ab
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-12-05 18:57:55 +0530

    Delete AreaOfRectangle.c

--- 1. Basics/AreaOfRectangle.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a,b;
-    printf("\nEnter the length and bredth of a rectangle: ");
-    scanf("%d %d",&a,&b);
-    printf("The area of rectangle is:%d sruare units\n",a*b);
-    return 0;
-}
