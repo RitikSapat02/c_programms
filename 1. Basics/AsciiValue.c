8e2640a379a5750a64a194f98e3399159230f91d
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-12-09 18:38:49 +0530

    Add files via upload

---
+++ 1. Basics/AsciiValue.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character:\n");
+    scanf("%c",&ch);
+    printf("The ASCII value of a chaaracter %c is %d\n",ch,ch);
+    return 0;
+}
